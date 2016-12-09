fe79e81145f3efadaaaa2b69d358319733270a33
Alec NEwman <alecnwmn904@gmail.com>
2016-12-08 23:26:38 -0500

    Changed the style

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function print_remaining(s) {
     return min + ':' + sec;
 }
 
-function start(n_rounds, sections) {
+function start(n_rounds, points) {
     var start_elem = document.getElementById('start');
     var timer_elem = document.getElementById('timer');
     start_elem.style.display = 'none';
@@ -22,22 +22,17 @@ function start(n_rounds, sections) {
     function pie(start, end) {
         start = 2*Math.PI*(start - 0.25);
         end = 2*Math.PI*(end - 0.25);
-
-        ctx.moveTo(100, 100);
-        ctx.arc(100, 100, 100, start, end);
-        ctx.moveTo(100, 100);
     }
 
     var timer_elem = document.getElementById('timer');
-    var last_position = 0;
     var round_length = 0;
     var need_refresh = true;
     var commandElem = document.getElementById('command');
     var remaining_elem = document.getElementById('remaining');
     var commandText = commandElem.innerText;
 
-    for (var i = 0; i < sections.length; i++) {
-        round_length += sections[i].length;
+    for (var i = 0; i < points.length; i++) {
+        round_length += points[i].length;
     }
 
     var total_time = round_length * n_rounds;
@@ -47,7 +42,7 @@ function start(n_rounds, sections) {
 
     function paint() {
         var elapsed = (+new Date() - beginTime)/1000;
-        var position = elapsed/round_length % 1;
+        var position = elapsed % round_length;
         var remaining = (total_time - elapsed)|0;
 
         if (remaining <= 0) {
@@ -62,34 +57,36 @@ function start(n_rounds, sections) {
         }
 
         var start = 0;
+        for (var i = 0; i < points.length; i++) {
+            var point = points[i];
+            var next_point = points[(i + 1)%points.length];
+            var end = start + point.length;
 
-        ctx.clearRect(0, 0, 200, 200);
-        for (var i = 0; i < sections.length; i++) {
-            var end = start + sections[i].length/round_length;
-            if (position > start && position <= end && sections[i].name !== commandText) {
-                commandElem.innerText = commandText = sections[i].name;
+            if (position > start && position <= end) {
+                if (point.name !== commandText) {
+                    commandElem.innerText = commandText = point.name;
+                }
+
+                var breath = point.breath + (next_point.breath - point.breath) * (position - start) / (end - start);
+
+                ctx.fillStyle = point.color;
+                ctx.beginPath();
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.arc(100, 100, 100*breath, 0, 2*Math.PI);
+                ctx.fill();
             }
-            ctx.fillStyle = sections[i].color;
-            ctx.beginPath();
-            pie(start, end);
-            ctx.fill();
+
             start = end;
         }
-
-        ctx.fillStyle = 'rgba(255,255,255,0.5)';
-        ctx.beginPath();
-        pie(0, position);
-        ctx.fill();
-        last_position = position;
     }
 
     var timer = setInterval(requestAnimationFrame.bind(null, paint), 1000/47);
 }
 
 document.getElementById('start-btn').onclick = function() {
-    start(1, [
-        {name: 'Inhale', length: 3, color: 'blue'},
-        {name: 'Exhale', length: 9, color: 'green'},
+    start(32, [
+        {name: 'Inhale', length: 3, breath: 0, color: 'black'},
+        {name: 'Exhale', length: 9, breath: 1, color: 'black'},
     ]);
 }
 
